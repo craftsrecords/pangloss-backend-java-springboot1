e5b548c0f6a0e7f77b38a0e63139cb3db2be4b81
Julien Topçu <julien.topcu@gmail.com>
2018-11-18 23:13:52 +0100

    Initializing Spring Boot infrastructure

---
+++ src/main/java/org/owasp/pangloss/PanglossApplication.java
@@ -0,0 +1,12 @@
+package org.owasp.pangloss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanglossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanglossApplication.class, args);
+	}
+}
