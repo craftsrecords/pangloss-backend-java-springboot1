73615b4a11eba8ca058b4d12f3d84233fa58cec3
Julien Topçu <julien.topcu@gmail.com>
2018-12-23 18:43:50 +0100

    Adding basic authentication

--- src/main/java/org/owasp/pangloss/infra/controller/ProfileController.java
+++ src/main/java/org/owasp/pangloss/infra/controller/ProfileController.java
@@ -5,11 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController("profile")
 public class ProfileController {
 
     @GetMapping
-    public ResponseEntity<User> profile() {
-        return ResponseEntity.ok(new User("poc-user"));
+    public ResponseEntity<User> profile(Principal principal) {
+        return ResponseEntity.ok(new User(principal.getName()));
     }
 }
--- src/test/java/org/owasp/pangloss/infra/controller/ProfileControllerTests.java
+++ src/test/java/org/owasp/pangloss/infra/controller/ProfileControllerTests.java
@@ -4,17 +4,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.owasp.pangloss.infra.controller.ProfileControllerTests.USERNAME;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ProfileController.class)
+@WithMockUser(username = USERNAME)
 public class ProfileControllerTests {
 
+    static final String USERNAME = "poc-user";
     @Autowired
     private MockMvc mockMvc;
 
@@ -22,7 +26,7 @@ public class ProfileControllerTests {
     public void should_return_poc_user() throws Exception {
         mockMvc.perform(get("/profile"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"name\":\"poc-user\"}"));
+                .andExpect(content().json("{\"name\":\"" + USERNAME + "\"}"));
     }
 
 }
