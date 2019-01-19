7c8a8ca4c2c35f33ddb239b9092e2158459da927
Julien Topçu <julien.topcu@gmail.com>
2019-01-19 15:47:53 +0100

    Changing POC infrastructure

---
+++ src/main/java/org/owasp/pangloss/infra/security/PocAuthenticationProvider.java
@@ -0,0 +1,39 @@
+package org.owasp.pangloss.infra.security;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("poc")
+public class PocAuthenticationProvider implements AuthenticationProvider {
+
+    private static final String USERNAME = "poc-user";
+    private static final String PASSWORD = "poc-pwd";
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String name = authentication.getName();
+        Object credentials = authentication.getCredentials();
+
+        if (areCredentialsCorrect(name, credentials)) {
+            return new UsernamePasswordAuthenticationToken(name, credentials.toString(), null);
+        }
+
+        throw new BadCredentialsException("Unauthorized");
+    }
+
+    private boolean areCredentialsCorrect(String name, Object credentials) {
+        return USERNAME.equals(name) && PASSWORD.equals(credentials);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
---
+++ src/test/java/org/owasp/pangloss/infra/security/PocAuthenticationProviderTest.java
@@ -0,0 +1,47 @@
+package org.owasp.pangloss.infra.security;
+
+import org.junit.Test;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PocAuthenticationProviderTest {
+
+    private final PocAuthenticationProvider pocAuthenticationProvider = new PocAuthenticationProvider();
+
+
+    @Test
+    public void should_authenticate_poc_user() {
+        //Given
+        String username = "poc-user";
+        String password = "poc-pwd";
+        Authentication authentication = new TestingAuthenticationToken(username, password);
+        //When
+        Authentication result = pocAuthenticationProvider.authenticate(authentication);
+        //Then
+        assertThat(result).isInstanceOf(UsernamePasswordAuthenticationToken.class);
+        assertThat(result.getName()).isEqualTo(username);
+        assertThat(result.getCredentials()).isEqualTo(password);
+    }
+
+    @Test
+    public void should_throw_BadCredentialsException_for_unrecognized_credentials() {
+        //Given
+        Authentication authentication = new TestingAuthenticationToken("qsd", "mpiohzer");
+        //Then
+        assertThatThrownBy(() -> pocAuthenticationProvider.authenticate(authentication))
+                .isInstanceOf(BadCredentialsException.class);
+    }
+
+    @Test
+    public void should_supports_UsernamePasswordAuthenticationToken() {
+        //When
+        boolean supports = pocAuthenticationProvider.supports(UsernamePasswordAuthenticationToken.class);
+        //Then
+        assertThat(supports).isTrue();
+    }
+}
