c1e5a37debd551b4c20083e0d5adbe444c4960ee
Julien Topçu <julien.topcu@gmail.com>
2019-04-03 22:15:42 +0200

    Refactoring BCryptDemo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into users (username, password) values ('admin', 'admin');
-insert into users (username, password) values ('user', 'pwd');
+insert into users (username, password) values ('user',  'pwd');
 
 insert into authorities (username, authority) values ('admin', 'ADMIN');
 insert into authorities (username, authority) values ('user', 'USER');
--- src/test/java/org/owasp/pangloss/infra/security/HashedPasswordBCryptDemo.java
+++ src/test/java/org/owasp/pangloss/infra/security/HashedPasswordBCryptDemo.java
@@ -3,23 +3,28 @@ package org.owasp.pangloss.infra.security;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.stream.Stream;
+
 import static java.lang.String.format;
 import static java.util.stream.IntStream.range;
 
 public class HashedPasswordBCryptDemo {
 
     public static void main(String[] args) {
-        String password = "admin";
 
         PasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        range(1, 5)
-                .forEach(value -> {
+        Stream.of("admin", "pwd").forEach((password) -> {
+                    range(1, 5)
+                            .forEach(value -> {
 
-                    String encodedPassword = encoder.encode(password);
-                    boolean matches = encoder.matches(password, encodedPassword);
+                                String encodedPassword = encoder.encode(password);
+                                boolean matches = encoder.matches(password, encodedPassword);
 
-                    System.out.println(format("%s matches %s => %s", encodedPassword, password, matches));
-                });
+                                System.out.println(format("%s matches %s => %s", encodedPassword, password, matches));
+                            });
+                    System.out.println();
+                }
+        );
     }
 }
