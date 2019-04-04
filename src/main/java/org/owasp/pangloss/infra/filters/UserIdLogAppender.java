84af08af4286598d8b19c92bdbb0d019bce203a9
Julien Topçu <julien.topcu@gmail.com>
2019-04-04 16:11:11 +0200

    Adding the userid in the request by default

--- src/main/java/org/owasp/pangloss/infra/filters/UserIdLogAppender.java
+++ src/main/java/org/owasp/pangloss/infra/filters/UserIdLogAppender.java
@@ -1,9 +1,17 @@
 package org.owasp.pangloss.infra.filters;
 
 import org.springframework.core.annotation.Order;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Component
@@ -16,7 +24,14 @@ public class UserIdLogAppender implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        //appendid
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            //Simulating a userId by taking the hash of the user name, to avoid to develop a userDetailsService.
+            //Because we don't want to log the name of the user, think about GDPR.
+            int userId = authentication.getName().hashCode();
+            request.getSession().setAttribute("userId", userId);
+        }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
